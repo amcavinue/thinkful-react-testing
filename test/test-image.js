9f07f6efcd90e852728512e5e5ca77532ae13064
Aaron <aaronmcavinue@gmail.com>
2016-12-20 01:41:28 +0000

    Add tests for gallery component.

--- test/test-image.js
+++ test/test-image.js
@@ -3,6 +3,7 @@ var TestUtils = require('react-addons-test-utils');
 var should = require('chai').should();
 
 var Image = require('../js/components/image');
+var Gallery = require('../js/components/gallery');
 
 describe('Image component', function() {
     it('Renders the image and description',  function() {
@@ -22,5 +23,32 @@ describe('Image component', function() {
         var p = result.props.children[1];
         p.type.should.equal('p');
         p.props.children.should.equal(description);
+    });
+});
+
+describe('Gallery component', function() {
+    it('Renders the images with props', function() {
+        var renderer = TestUtils.createRenderer();
+        renderer.render(<Gallery 
+            images={
+                [
+                    {
+                        url: 'www.abc.com/image.jpg',
+                        description: 'abc image'
+                    },
+                    {
+                        url: 'www.def.com/image.jpg',
+                        description: 'def image'
+                    }
+                ]
+                
+            } />);
+        var result = renderer.getRenderOutput();
+        result.props.className.should.equal('gallery');
+        result.props.children.length.should.equal(2);
+        
+        var img1 = result.props.children[0];
+        img1.props.url.should.equal('www.abc.com/image.jpg');
+        img1.props.description.should.equal('abc image');
     });
 });
